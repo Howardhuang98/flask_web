8c69b5427b85662d6e266e509388ff4a9eca3c39
HUANG HAO <934303306@qq.com>
2021-08-18 16:04:50 +0800

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-#encoding: utf-8
+# encoding: utf-8
 
 from flask import Flask, render_template, request, redirect, url_for, session
+
 import config
-from models import User, Question, Answer
-from exts import db
 from decorators import login_required
-from flask_migrate import Migrate
-from sqlalchemy import or_
+from exts import db
+from models import User, Message
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -20,56 +19,31 @@ app.config.from_object(config)
 db.init_app(app)
 
 
-
-@app.route('/')
-def index():
-    # context = {
-    #     'questions': Question.query.order_by('-create_time').all()
-    # }
-    return render_template('index.html')
-
-
-@app.route('/login/', methods=['GET', 'POST'])
+# 主页，默认情况是不登陆无法进入
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        message = "欢迎~"
+        return render_template('login.html', message=message)
     else:
-        telephone = request.form.get('telephone')
+        username = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter(User.telephone == telephone, User.password ==
+        user = User.query.filter(User.username == username, User.password ==
                                  password).first()
         if user:
             session['user_id'] = user.id
-            # 如果想在31天内都不需要登录
             session.permanent = True
             return redirect(url_for('index'))
         else:
-            return u'手机号码或者密码错误，请确认好在登录'
+            return redirect('/')
 
-@app.route('/regist/', methods=['GET', 'POST'])
-def regist():
-    if request.method == 'GET':
-        return render_template('regist.html')
-    else:
-        telephone = request.form.get('telephone')
-        username = request.form.get('username')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
 
-        # 手机号码验证，如果被注册了就不能用了
-        user = User.query.filter(User.telephone == telephone).first()
-        if user:
-            return u'该手机号码被注册，请更换手机'
-        else:
-            # password1 要和password2相等才可以
-            if password1 != password2:
-                return u'两次密码不相等，请核实后再填写'
-            else:
-                user = User(telephone=telephone, username=username, password=password1)
-                db.session.add(user)
-                db.session.commit()
-                # 如果注册成功，就让页面跳转到登录的页面
-                return redirect(url_for('login'))
+@app.route('/index')
+@login_required
+def index():
+    messages = Message.query.order_by(Message.create_time.desc()).limit(10).all()
+    return render_template('index.html', messages=messages)
+
 
 # 判断用户是否登录，只要我们从session中拿到数据就好了   注销函数
 @app.route('/logout/')
@@ -79,52 +53,21 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+
 @app.route('/question/', methods=['GET', 'POST'])
 @login_required
 def question():
     if request.method == 'GET':
-        return render_template('question.html')
+        return render_template('message.html')
     else:
         title = request.form.get('title')
         content = request.form.get('content')
-        question = Question(title=title, content=content)
+        message = Message(title=title, content=content)
         user_id = session.get('user_id')
-        user = User.query.filter(User.id == user_id).first()
-        question.author = user
-        db.session.add(question)
+        message.author_id = user_id
+        db.session.add(message)
         db.session.commit()
         return redirect(url_for('index'))
-
-@app.route('/detail/<question_id>/')
-def detail(question_id):
-    question_model = Question.query.filter(Question.id == question_id).first()
-    return render_template('detail.html', question=question_model)
-
-@app.route('/add_answer/', methods=['POST'])
-@login_required
-def add_answer():
-    content = request.form.get('answer_content')
-    question_id = request.form.get('question_id')
-    answer = Answer(content=content)
-    user_id = session['user_id']
-    user = User.query.filter(User.id == user_id).first()
-    answer.author = user
-    question = Question.query.filter(Question.id == question_id).first()
-    answer.question = question
-    db.session.add(answer)
-    db.session.commit()
-    return redirect(url_for('detail', question_id=question_id))
-
-@app.route('/search/')
-def search():
-    q = request.args.get('q')
-    # title, content
-    # 或 查找方式（通过标题和内容来查找）
-    # questions = Question.query.filter(or_(Question.title.contains(q),
-    #                                     Question.content.constraints(q))).order_by('-create_time')
-    # 与 查找（只能通过标题来查找）
-    questions = Question.query.filter(Question.title.contains(q), Question.content.contains(q))
-    return render_template('index.html', questions=questions)
 
 
 # 钩子函数(注销)
@@ -137,6 +80,6 @@ def my_context_processor():
             return {'user': user}
     return {}
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 from datetime import datetime
+
 from exts import db
 
 
@@ -8,7 +9,7 @@ class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     telephone = db.Column(db.String(11), nullable=False)
-    username = db.Column(db.String(50), nullable=False,unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
     level = db.Column(db.Integer, nullable=False)
 
@@ -24,10 +25,8 @@ class Message(db.Model):
     # now()获取的是服务器第一次运行的时间
     # now就是每次创建一个模型的时候，都获取当前的时间
     create_time = db.Column(db.DateTime, default=datetime.now)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('User.id'))
     author = db.relationship('User', backref=db.backref('Message'))
 
     def __str__(self):
         return self.title
-
-
